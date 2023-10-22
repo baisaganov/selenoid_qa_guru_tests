41935efd4cf1f44cd7e8e89e228389ef148b01e5
Alisher Baisaganov <baisaganov99@gmail.com>
2023-10-22 21:52:40 +0600

    attachments

--- src/test/java/allure/TestBaseExtended.java
+++ src/test/java/allure/TestBaseExtended.java
@@ -1,6 +1,8 @@
 package allure;
 
 import com.codeborne.selenide.Configuration;
+import helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -12,14 +14,22 @@ public class TestBaseExtended {
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "http://31.129.109.167:8080/wd/hub";
         Configuration.browser = "chrome";
-        Configuration.browserVersion = "103.0";
+        Configuration.browserVersion = "104.0";
 
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
+//        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+//        desiredCapabilities.setCapability("selenoid:options", Map.<String, Object>of(
+//                "enableVNC", true,
+//                "enableVideo", true
+//        ));
+//
+//        Configuration.browserCapabilities = desiredCapabilities;
+    }
 
-        Configuration.browserCapabilities = desiredCapabilities;
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+//        Attach.getVideoUrl();
     }
 }
