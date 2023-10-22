7e9042fb473416b1ac31d922a26d2d4cc9217f5a
Alisher Baisaganov <baisaganov99@gmail.com>
2023-10-22 23:07:59 +0600

    attachments video

--- src/test/java/allure/TestBaseExtended.java
+++ src/test/java/allure/TestBaseExtended.java
@@ -4,12 +4,7 @@ import com.codeborne.selenide.Configuration;
 import helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class TestBaseExtended {
     @BeforeAll
@@ -18,23 +13,6 @@ public class TestBaseExtended {
         Configuration.remote = "http://31.129.109.167:8080/wd/hub";
         Configuration.browser = "chrome";
         Configuration.browserVersion = "104.0";
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("enableVNC", true);
-//        capabilities.setCapability("enableVideo", true);
-//        Configuration.browserCapabilities = capabilities;
-
-
-//        ChromeOptions options = new ChromeOptions();
-//        options.setCapability("browserVersion", "104.0");
-//        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
-//            /* How to set session timeout */
-//            put("sessionTimeout", "1m");
-//
-//            /* How to enable video recording */
-//            put("enableVideo", true);
-//        }});
-//
-//        Configuration.browserCapabilities = options;
     }
 
     @AfterEach
